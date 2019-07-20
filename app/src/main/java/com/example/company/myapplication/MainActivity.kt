c226f528ab62f41eee841465804b0a21ec1ab898
Artur_Nurtdinov <a.nurtdnow@yandex.ru>
2019-07-20 23:39:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.company.myapplication
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val data = MutableList(11) { x -> "${x * x}" }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        val listView = findViewById<ListView>(R.id.listView)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            data.add((view as TextView).text.toString())
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
